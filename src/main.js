03f1c993260f5aa8d551159776939fe22304ad78
Patryk Mikołajczak <patryk.mikolajczak@everest-it.com>
2018-11-20 23:40:37 +0100

    Image preload and infinity scroll test

--- src/main.js
+++ src/main.js
@@ -2,8 +2,36 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+
+Vue.prototype.$http = axios
+
+axios.defaults.headers.post['crossDomain'] = true;
 
 Vue.config.productionTip = false
+
+Vue.directive('img', {
+    inserted: (el, url) => {
+        var img = new Image()
+        img.src = url.value
+        el.style.opacity = 1
+        el.style.width = '200px'
+        el.style.height = '200px'
+        el.style.transition = 'all 1s'
+
+        img.onload = (photo) => {
+            el.src = url.value;
+            // setTimeout( () => {
+            //     el.style.opacity = 0
+                el.style.width = (photo.target.width / (photo.target.height / 200) ) + 'px'
+            //     setTimeout( () => {
+            //     el.style.opacity = 1
+            //     },100)
+            // }, 100)
+            // el.style.animate = 'opacity 1 1s'
+        }
+    }
+});
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,80 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+        <div
+            v-for="(p,i) in photos_list"
+            :key="i"
+            class="preloaded-img"
+        >
+                <img 
+                    src="../assets/logo.png" 
+                    :alt="'photo' + i" 
+                    v-img="p"
+                />
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+    name: 'home',
+    data() {
+        return {
+            photos_list: [],
+            loading: false,
+            ticking: false
+        }
+    },
+    created() {
+        this.getData()
+        window.addEventListener('scroll', this.changeScroll)
+    },
+    beforeDestroy() {
+        window.removeEventListener('scroll', this.changeScroll)
+    },
+    methods: {
+        changeScroll(e) {
+            const last_known_scroll_position = window.scrollY
+            if (!this.ticking) {
+
+                window.requestAnimationFrame( () => {
+                    if(!this.loading) {
+                        let maxScroll = window.scrollMaxY || (document.documentElement.scrollHeight - document.documentElement.clientHeight)
+                        if(last_known_scroll_position * 100 / maxScroll > 90) {
+                            this.getData()
+                        }
+                    }
+                    this.ticking = false
+                })
+                
+                this.ticking = true
+
+            }
+        },
+        getData() {
+            this.loading = true
+            this.$http.get('http://localhost:666/http://shibe.online/api/shibes?count=16')
+                .then( ( resp ) => {
+                    this.photos_list = [...this.photos_list, ...resp.data]
+                    this.loading = false
+                })
+                .catch( err => {
+                    console.error( err )
+                    this.loading = false
+                })
+        }
+    }
 }
 </script>
+
+<style lang="scss" scoped>
+    .preloaded-img {
+        display: inline;
+        img {
+            height: 200px;
+        }
+    }
+</style>
