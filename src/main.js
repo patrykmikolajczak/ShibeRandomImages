aef2b4f1011b0628ea1be209247ccf8b5f2aaeed
Patryk Mikołajczak <patryk.mikolajczak@everest-it.com>
2018-11-23 00:06:43 +0100

    Margin fix

--- src/main.js
+++ src/main.js
@@ -26,11 +26,19 @@ Vue.directive('img', {
         img.onload = (photo) => {
             el.src = url.value;
             if(photo.target.width > photo.target.height) {
-                el.style.width = (photo.target.width / (photo.target.height / 200) ) + 'px'
+                let width = (photo.target.width / (photo.target.height / 200) )
+                el.style.width = width + 'px'
                 el.style.height = "200px"
+                if(width > 200) {
+                    el.style['margin-left'] = '-' + ((width - 200) / 2) + 'px'
+                }
             } else {
+                let height = (photo.target.height / (photo.target.width / 200) )
                 el.style.width = "200px"
-                el.style.height = (photo.target.height / (photo.target.width / 200) ) + 'px'
+                el.style.height = height + 'px'
+                if(height > 200) {
+                    el.style['margin-top'] = '-' + ((height - 200) / 2) + 'px'
+                }
             }
             // el.style.width = (photo.target.width / (photo.target.height / 200) ) + 'px'
         }
